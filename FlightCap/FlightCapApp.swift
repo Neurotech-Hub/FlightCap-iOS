9027b320727c474f85151ac3fdf9203b46263062
Matt Gaidica, Ph.D <matt@gaidi.ca>
2026-05-20 11:11:49 -0500

    Initial Commit

---
+++ FlightCap/FlightCapApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightCapApp.swift
+//  FlightCap
+//
+//  Created by Matt Gaidica on 5/20/26.
+//
+
+import SwiftUI
+
+@main
+struct FlightCapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlightCapTests/FlightCapTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlightCapTests.swift
+//  FlightCapTests
+//
+//  Created by Matt Gaidica on 5/20/26.
+//
+
+import Testing
+@testable import FlightCap
+
+struct FlightCapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
